03c05f4ebe516398e23440acab5ec9d2c2106033
Lucia2135 <103713537+Lucia2135@users.noreply.github.com>
2022-05-06 12:37:40 +0900

    Update studycafe.c

--- studycafe.c
+++ studycafe.c
@@ -1,2 +1,26 @@
 "studycafe.h"
 
+int updateReseveration(Reseveration *s){
+    printf("\n");
+    printf("이름: ");
+    scanf(" %[^\n]s",s->name);
+
+    printf("전화번호 뒷자리: ");
+    scanf("%d",&s->phone_no);
+
+    printf("예약 자리: ");
+    scanf(" %[^\n]s",s->space);
+  
+    printf("예약 시간: ");
+    scanf(" %[^\n]s",s->during);
+  
+    printf("==> 수정됨!\n");
+    return 1;
+}
+
+int deleteReseveration(Reseveration *s){
+ 
+    s->phone_no=-1;
+    printf("==> 삭제됨!\n");
+    return 0;
+}
