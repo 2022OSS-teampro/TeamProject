f72a27aeec5c9e8d95977789a62a794c77d45d47
Hanss <kogmaw1253@gmail.com>
2022-05-09 23:56:06 +0900

    main.c 파일 추가

---
+++ main.c
@@ -0,0 +1,28 @@
+#include "studycafe.h"
+
+int main(){
+    int menu;
+
+    while(1){
+        menu = selectMenu();
+
+        if(menu == 1){
+
+        }
+        else if(menu == 2){
+
+        }
+        else if(menu =3){
+
+        }
+        else if(menu == 4){
+
+        }
+
+
+        else if( menu == 0){
+            printf("\n 종료되었습니다! \n");
+        }
+    }
+    return 0;
+};
