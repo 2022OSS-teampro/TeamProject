c06dff5580b64ae41fdb0f27a8693de62a64b256
Hanss <kogmaw1253@gmail.com>
2022-05-14 13:43:29 +0900

    sale 기능 관련 함수 sale.c로 이동

--- studycafe.c
+++ studycafe.c
@@ -85,23 +85,7 @@ void saveData(Reseveration *s, int count);
 int loadData(Reseveration *s);
 void searchName(Reseveration *s, int count);
 
-//할인 이벤트 관련 기능 --> 제가 이부분 하겠습니다!, 한결
-void saleEventShow(){
-}
-int selectSaleNo(){
-    int no;
-    printf("\n==== 현재 할인 이벤트 ====\n");
-    printf("\n<현재 할인 이벤트 상황>\n");
-    saleEventShow();
-    printf("1. 할인 이벤트 추가");
-    printf("2. 할인 이벤트 수정");
-    printf("0. menu로 돌아가기")
 
-    scanf("%[^\n]d", &no);
-    return no;
-}
-void addEvent();
-void updateEvent();
 
 //시간대별 가격 보여주기
 void showPrice();
