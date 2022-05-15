8a238830e2507e029cb58a8f85d7800b849cca09
Hanss <kogmaw1253@gmail.com>
2022-05-15 16:22:12 +0900

    할인이벤트 관련 함수 추가

--- Sale.c
+++ Sale.c
@@ -4,22 +4,43 @@
     //할인 이벤트 관련 구조체
     char during[100];
     int saleRate; // 할인율
+    bool isSale = false;
 }Sale;
 */
 
-void saleEventShow(){
-}
+void saleEventShow(Sale s){
+    if(s.isSale == 0){
+        printf("\n현재 할인 이벤트 없음\n"ㅏ);
+        return;
+    }
+    else{
+    printf("이벤트 기간 : %s\n", s.during);
+    printf("할인율 : %d %\n", &s.saleRate);
+    }
+};
 int selectSaleNo(){
     int no;
     printf("\n==== 현재 할인 이벤트 ====\n");
     printf("\n<현재 할인 이벤트 상황>\n");
     saleEventShow();
-    printf("1. 할인 이벤트 추가");
-    printf("2. 할인 이벤트 수정");
-    printf("0. menu로 돌아가기")
+    printf("1. 할인 이벤트 추가\n");
+    printf("2. 할인 이벤트 수정\n");
+    printf("0. menu로 돌아가기\n");
 
-    scanf("%[^\n]d", &no);
+    scanf("%d", &no);
     return no;
 }
-void addEvent();
-void updateEvent();
+void addEvent(Sale s){
+    printf("할인 기간을 입력해주세요 > \n");
+    scanf("%[^\n]s", s.during);
+    printf("할인율 : ");
+    scanf("%d", &s.saleRate);
+    s.isSale = 1;
+}
+void updateEvent(Sale s){
+    printf("할인 기간을 입력해주세요 > \n");
+    scanf("%[^\n]s", s.during);
+    printf("할인율 : ");
+    scanf("%d", &s.saleRate);
+    s.isSale = 1;
+}
