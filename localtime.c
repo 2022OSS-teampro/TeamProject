93524f37d9176cb3fc275cb5a80926426c20bc5b
Hanss <kogmaw1253@gmail.com>
2022-05-16 08:30:14 +0900

    시간관련 기능 추가를 위한 헤더파일 추가

--- localtime.c
+++ localtime.c
@@ -1,3 +1,7 @@
 #include "localtime.h"
 //시간 관련 함수
 
+void userImpendingEnd(Reseveration *s){
+    struct tm t = *localtime(&time(NULL)));
+    if(calculatorTime(s->during, t) <= 1)
+}
--- localtime.h
+++ localtime.h
@@ -3,3 +3,6 @@
 
 int calculatorTime(char duration[], struct tm currentTime);
 int isOkayDuration(int gapOfTime);
+
+//임박한 사용자 출력
+void userImpendingEnd(Reseveration *s);
